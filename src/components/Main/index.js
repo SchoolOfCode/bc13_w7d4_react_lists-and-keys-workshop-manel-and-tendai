f70fcafb7a6db9da061776b2e39f0a88657f44f9
Tendai Karuwo <tendaikaruwo96@gmail.com>
2022-11-10 16:24:56 +0000

    task 3 complete

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,11 +6,13 @@ import samplePosts from "../../libs/data";
 
 import Post from "../Post";
 
+const {postId, title, date, author, text, highlights, image} = samplePosts
+
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
   {
     posts.map(({ Post }) => {
-      return <Post key={postId} />;
+      return <Post key={postId} title={title} date={date} author={author} text={text} highlights={highlights}  image={image}/>;
     });
   }
 
