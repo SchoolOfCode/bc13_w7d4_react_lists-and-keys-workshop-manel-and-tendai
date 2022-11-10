819c104ec2fed1a84a52b2c4d9c9e976e10ce942
manelduv <duval.manel@gmail.com>
2022-11-10 16:04:09 +0000

    Task 2

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
-  {posts.map(({Post})=> {
-    return <Post />
-    } )
-
+  {
+    posts.map(({ Post }) => {
+      return <Post key={postId} />;
+    });
   }
 
   return <main id="main"></main>;
